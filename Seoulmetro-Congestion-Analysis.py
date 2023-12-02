629420a43059e07a72ac98b94680e332671c9d93
JiinHong <com5942@seoultech.ac.kr>
2023-12-02 17:17:45 +0900

    add license

--- Seoulmetro-Congestion-Analysis.py
+++ Seoulmetro-Congestion-Analysis.py
@@ -1,3 +1,7 @@
+# MIT License
+# Copyright (c) 2023 박진홍
+# 이 코드는 MIT 라이센스에 따라 배포됩니다.
+
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,49 +12,47 @@ from sklearn.preprocessing import PolynomialFeatures
 warnings.filterwarnings("ignore", category=RuntimeWarning, message="Mean of empty slice")
 np.seterr(divide='ignore', invalid='ignore')
 
-# Load data from CSV file
-data = []  # List to store the data
+# CSV 파일에서 데이터 불러오기
+data = []  # 데이터를 저장할 리스트
 with open('Seoulmetro_Congestion_by_station_and_time_zone.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
-    header = next(reader)  # Read the header row
+    header = next(reader)  # 헤더 행 읽기
     for row in reader:
-        # Replace empty strings with None
+        # 빈 문자열이나 None을 0으로 대체
         row = [0 if value == '' or value == None else value for value in row]
         data.append(row)
 
-# Extract necessary information
-lines = [row[2] for row in data]  # Line column
+# 필요한 정보 추출
+lines = [row[2] for row in data]
 DayOfWeek = [row[1] for row in data]
 times = np.array([list(map(float, row[6:])) for row in data])
-congestions = np.array([list(map(float, row[6:])) for row in data])  # Convert time data to a NumPy array
+congestions = np.array([list(map(float, row[6:])) for row in data])
 
-# Create a plot (e.g., for subway lines)
-time_labels = header[6:]  # Time slot labels
-
+# 지하철 노선별
+time_labels = header[6:] # 시간대 레이블
 
 x = np.arange(len(time_labels))
-# Extract and plot data for each subway line
+# 각 지하철 노선에 대한 데이터 추출
 
 user_input = int(input("Select the line of the subway you want to take (lines 1 to 8).\n If you want to see congestion statistics for the entire subway, enter 0.\n"))
 user_day = int(input("Choose today's day : 1.Weekday 2.Saturday 3.Holiday (Please input number)\n"))
 if user_input != 0:
     user_time_x, user_time_y = map(int, input("Enter the current time between 5:30 today and 00:30 the next day. (ex. 17:30) \n").split(":"))
     convert_to_input = user_time_x*2 + user_time_y/60 - 10.5
-
     if convert_to_input < 0:
         convert_to_input += 48
 
 if user_input == 0:
     plt.figure(figsize=(12, 6))
-    for line in range(1, 9):  # Iterate over subway lines 1 to 8
+    for line in range(1, 9):  # 1부터 8까지의 지하철 노선 반복하며 전체 혼잡도 그래프 그리기
         if user_day == 1:
             mask = (np.array(lines) == str(line)) & (np.array(DayOfWeek) == "weekday")
         elif user_day == 2:
             mask = (np.array(lines) == str(line)) & (np.array(DayOfWeek) == "Saturday")
         elif user_day == 3:
             mask = (np.array(lines) == str(line)) & (np.array(DayOfWeek) == "holiday")
-        congestion = np.mean(times[mask][:, :len(time_labels)], axis=0)  # Use the minimum time slots available
-        congestion = [val for val in congestion if val is not None and val != 0]  # Remove None and 0 values from congestion data
+        congestion = np.mean(times[mask][:, :len(time_labels)], axis=0)
+        congestion = [val for val in congestion if val is not None and val != 0]
         plt.plot(x[:len(congestion)], congestion, label=f'Line {line}')
     plt.xticks(x[:39], time_labels[:39], rotation=90)
     plt.xlabel('Time Slot')
